c17c6585633ba8ad4833a9a7ac1ccb2304311d8d
Albassir <albassir02@gmail.com>
2016-11-05 17:04:54 -0400

    Comptes (#1)
    * created clients class - client.cs
    
    * creation des methodes de la class comptes, creation header

---
+++ INF731-TP2/INF731-TP2/clients.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF731_TP2
+{
+    class clients
+    {
+            /// Class pour gérere les clients
+            /// Par Olivier Contant
+    }
+}
