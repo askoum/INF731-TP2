6cdf504682934b6a291eaee552b7e1799b39304d
Olivier Contant <ocontant@users.noreply.github.com>
2016-11-05 17:24:48 -0400

    Dev (#4)
    * test adding new file
    
    * Comptes (#1)
    
    * created clients class - client.cs
    
    * creation des methodes de la class comptes, creation header
    
    * Fixed corruption in file .csproj
    
    * fixed corruption in .csproj

---
+++ INF731-TP2/INF731-TP2/comptes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF731_TP2
+{
+   class comptes
+   {
+   }
+   /// les methodes de la class comptes bancaire
+   /// et  ses class derivées (compte chèque,compte flexible et comptes épargne)
+}
